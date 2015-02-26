42e3db049347033134f0f9765c4f959009d1aa5a
thalmr <timothy.r.sadler@gmail.com>
2015-02-26 07:32:03 +0000

    Created application and added devise gemfile with basic config steps

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prospecto::Application.config.secret_token = '8ca2a7a1c72961a4301aac93a9d5bea1e635f9ec60b6df3a2bc627d1b3d676318f040c6f91bbd76733151c70ad98bfbcaac8a61c141341859350f4bc62f14749'
